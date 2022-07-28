c1d4d84480fc44793dc25e9192ef9cf5508f4982
phoenitunes <70701513+phoenitunes@users.noreply.github.com>
2022-07-28 17:31:49 +0100

    commons

--- Exercise Files/04 Collections/counter_start.py
+++ Exercise Files/04 Collections/counter_start.py
@@ -12,19 +12,39 @@ def main():
     class2 = ["Bill", "Barry", "Cindy", "Debbie", "Frank",
               "Gabby", "Kelly", "James", "Joe", "Sam", "Tara", "Ziggy"]
 
-    # TODO: Create a Counter for class1 and class2
+    # # TODO: Create a Counter for class1 and class2
+    c1= Counter(class1)
+    c2=Counter(class2)
+    print(c1,c2)
 
-    # TODO: How many students in class 1 named James?
+    # # TODO: How many students in class 1 named James?
+    # print(c1["James"])
 
-    # TODO: How many students are in class 1?
+    # # TODO: How many students are in class 1?
+    # print(sum(c1.values()))
 
-    # TODO: Combine the two classes
+    # # TODO: Combine the two classes
+    # c1.update(class2)
+    # print(c1)
 
-    # TODO: What's the most common name in the two classes?
+    # # TODO: What's the most common name in the two classes?
+    # c1_common= c1.most_common(3)
 
-    # TODO: Separate the classes again
+    # print(c1_common)
+    # # TODO: Separate the classes again
+    # c1.subtract(class2)
+    # print("c1",c1,(sum(c1.values())))
+    # print("c2",(sum(c2.values())))
 
     # TODO: What's common between the two classes?
+    commons=[]
+    for student in c1.keys():
+        if student in c2.keys():
+            commons.append(student)
+            
+    print(c1 & c2)        
+        
+    print(commons)     
 
 
 if __name__ == "__main__":

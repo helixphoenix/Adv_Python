4ea31d8c66b2769accae91b096682705edad68ab
phoenitunes <70701513+phoenitunes@users.noreply.github.com>
2022-08-10 17:48:22 +0100

    numeric calc

--- Exercise Files/05 Classes/compattrs_start.py
+++ Exercise Files/05 Classes/compattrs_start.py
@@ -6,29 +6,50 @@ class myColor():
         self.red = 50
         self.green = 75
         self.blue = 100
+        self.black=10500
 
     # TODO: use getattr to dynamically return a value
     def __getattr__(self, attr):
-        pass
+        if attr == "rgbcolor":
+            print(attr)
+            return (self.red, self.green, self.blue, self.black)
+        elif attr == "hexcolor":
+            return "#{0:02x}{1:02x}{2:02x}{3:03x}".format(self.red, self.green, self.blue, self.black)
+        else:
+            raise AttributeError
 
     # TODO: use setattr to dynamically return a value
     def __setattr__(self, attr, val):
-        super().__setattr__(attr, val)
+        if attr == "rgbcolor":
+            self.red= val[0]
+            self.green = val[1]
+            self.blue = val[2]
+            self.black= val[3]
+        else:    
+             super().__setattr__(attr, val)
 
     # TODO: use dir to list the available properties
     def __dir__(self):
-        pass
+        return ("red","green","blue","black","rgbcolor","hexcolor")
 
 def main():
     # create an instance of myColor
     cls1 = myColor()
     # TODO: print the value of a computed attribute
+    print(cls1.__getattr__("rgbcolor"))
+    print(cls1.hexcolor)
 
     # TODO: set the value of a computed attribute
 
+    cls1.rgbcolor = (125, 200, 86, 10500)
+    print(cls1.hexcolor)
+
     # TODO: access a regular attribute
+    print(cls1.black)
 
     # TODO: list the available attributes
+    
+    print(dir(cls1))
 
 
 if __name__ == "__main__":
--- Exercise Files/05 Classes/numeric_start.py
+++ Exercise Files/05 Classes/numeric_start.py
@@ -11,16 +11,17 @@ class Point():
 
     # TODO: implement addition
     def __add__(self, other):
-        pass
+        return Point(self.x+other.x, self.y+other.y)
 
     # TODO: implement subtraction
     def __sub__(self, other):
-        pass
+        return Point(self.x-other.x, self.y-other.y)
 
     # TODO: implement in-place addition
     def __iadd__(self, other):
-        pass
-
+        self.x += other.x
+        self.y += other.y
+        return self
 
 def main():
     # Declare some points
@@ -29,11 +30,17 @@ def main():
     print(p1, p2)
 
     # TODO: Add two points
-
+    print(p1+p2)
     # TODO: subtract two points
-
+    print( p1-p2)
     # TODO: Perform in-place addition
-
+    print(p1)
+    print(p2)
+    
+    
+    p1 += p2
+    print(p1)
+     
 
 if __name__ == "__main__":
     main()
